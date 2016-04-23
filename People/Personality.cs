74127aae6bbf03619bd6b42daac90a84d0c1fc4c
mejborn <buster@mejborn.it>
2016-04-23 12:32:31 +0200

    Changes to Personality.cs

--- People/Personality.cs
+++ People/Personality.cs
@@ -3,4 +3,13 @@ using System.Collections;
 
 class Personality
 {
+	int D,I,S,C;
+	public Personality()
+	{
+		Random rnd = new Random ();
+		this.D = rnd.Next (1, 10);
+		this.I = rnd.Next (1, 10);
+		this.S = rnd.Next (1, 10);
+		this.C = rnd.Next (1, 10);
+	}
 }
