9dc4f155caec3324ae45b242b19adbcf1108b3f3
Ricard Grove <ricard.grove@gmail.com>
2016-04-24 03:45:33 +0200

    MainMenu and PauseMenu

---
+++ MainMenu/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Canvas exitMenu;
+    public Canvas optionMenu; 
+    public Button playText;
+    public Button optionText;
+    public Button exitText;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        exitMenu = exitMenu.GetComponent<Canvas>();
+        playText = playText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        optionText = optionText.GetComponent <Button>();
+        exitMenu.enabled = false;
+    }
+
+    public void PlayPress()
+    {
+       SceneManager.LoadScene("MainLevel");
+    }
+
+    public void OptionPress()
+    {
+
+    }
+
+
+    public void ExitPress()
+    {
+        exitMenu.enabled = true;
+        playText.enabled = false;
+        exitText.enabled = false;
+        optionText.enabled = false; 
+    }
+
+    public void NoPress()
+    {
+        exitMenu.enabled = false;
+        playText.enabled = true;
+        exitText.enabled = true;
+        optionText.enabled = true;
+    }
+
+    public void YesPress()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ MainMenu/PauseMenu.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool isPause = false;
+
+    public Canvas pauseMenu;
+    public Canvas exitMenu;
+    public Button continueText;
+    public Button exitText;
+    public Button optionText;
+
+    void Start()
+    {
+        pauseMenu = pauseMenu.GetComponent<Canvas>();
+        exitMenu = exitMenu.GetComponent<Canvas>();
+        continueText = continueText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        optionText = optionText.GetComponent<Button>();
+        pauseMenu.enabled = false;
+        exitMenu.enabled = false;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPause = !isPause;
+            if (isPause)
+            {
+                Time.timeScale = 0;
+                pauseMenu.enabled = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseMenu.enabled = false;
+            }
+        }
+    }
+
+    public void ContinuePress()
+    {
+        isPause = !isPause;
+        if (isPause)
+        {
+            Time.timeScale = 0;
+            pauseMenu.enabled = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.enabled = false;
+        }
+    }
+
+    public void ExitPress()
+    {
+        exitMenu.enabled = true;
+        continueText.enabled = false;
+        exitText.enabled = false;
+        optionText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        exitMenu.enabled = false;
+        continueText.enabled = true;
+        exitText.enabled = true;
+        optionText.enabled = true;
+    }
+
+    public void YesPress()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
